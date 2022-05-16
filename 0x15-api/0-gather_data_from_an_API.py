5306c737ae2eb42d84de4ae605a7d35de1941aa2
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-16 15:20:24 -0700

    [JSONP API]

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" REST API JSONPlaceholder (USERS) """
+import requests as req
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = int(argv[1])
+
+    def req_user():
+        """ Request the user from the JSONPlaceholder  """
+        users_JSONP = req.get('https://jsonplaceholder.typicode.com/users').json()
+        user = {}
+        for elem in users_JSONP:
+            if elem["id"] == user_id:
+                user = dict(elem)
+                break
+        return user
+
+    def req_todos():
+        """ Request task completed of employe """
+        user = req_user()
+        todos = req.get('https://jsonplaceholder.typicode.com/todos').json()
+        user_tuto = []
+        user_tutos_complete = []
+        tasks_complete = 0
+        for elem in todos:
+            if elem["userId"] == user_id:
+                user_tuto.append(dict(elem))
+                if elem["completed"]:
+                    tasks_complete = tasks_complete + 1
+                    user_tutos_complete.append(dict(elem))
+        total_tasks = len(user_tuto)
+        print("Employee {} is done with tasks({}/{}):"
+              .format(user["name"], tasks_complete, total_tasks))
+        for task in user_tutos_complete:
+            print("\t {}".format(task["title"]))
+
+    req_todos()
