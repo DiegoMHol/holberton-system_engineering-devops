aa1c42e30d1170b2087c6f02c537e7fd9f501c9a
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-16 16:29:49 -0700

    [PEP8]

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,18 +9,18 @@ if __name__ == "__main__":
     todos = req.get('https://jsonplaceholder.typicode.com/todos').json()
     users = req.get('https://jsonplaceholder.typicode.com/users').json()
     user = {}
-    
+
     for elem in users:
         if elem["id"] == int(argv[1]):
             user = dict(elem)
             break
     user_todos = []
-    
+
     for elem in todos:
         if elem["userId"] == int(argv[1]):
             user_todos.append(dict(elem))
     csv_export = []
-    
+
     for elem in user_todos:
         strg = []
         strg.append(argv[1])
@@ -31,7 +31,7 @@ if __name__ == "__main__":
             strg.append(False)
         strg.append(elem["title"])
         csv_export.append(strg)
-    
+
     with open('{}.csv'.format(argv[1]), 'w', newline='') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for strg in csv_export:
