9da02be0e1520cb3e036648e5b01fc0eb039471f
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-31 07:40:54 -0700

    [REQUEST SUBS REDDIT]

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Reddit api, to return subs """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    urlr = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    request = requests.get(urlr, headers = {'User-agent': 'yourbot'},
+                           allow_redirects=False)
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
+    else:
+        return 0
+        
+
