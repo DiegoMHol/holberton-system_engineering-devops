30ec582b6e1b76f875ca9f71836202adb9947e59
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-31 08:07:39 -0700

    [FIX PEp8]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     urlr = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    request = requests.get(urlr, headers = {'User-agent': 'yourbot'},
+    request = requests.get(urlr, headers={'User-agent': 'yourbot'},
                            allow_redirects=False)
     if request.status_code == 200:
         return request.json().get("data").get("subscribers")
     else:
         return 0
-        
-
